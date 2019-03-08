5083cb6bde79b5c896dc0798debe950ebc58e33a
bbierman <bbierman7@gmail.com>
2019-03-08 12:45:14 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,41 +5,50 @@ $(document).ready(function() {
           //CRYSTAL ARRAY FOR BUTTONS
           var crystals = ["assets/images/crystal-1.png", "assets/images/crystal-2.png", "assets/images/crystal-3.png", "assets/images/crystal-4.png"];
           var random;
+          
           //Variable named crystalBtn equal to $("<button>")
         function renderimg() {
             
-        
          for (let i = 0; i < crystals.length; i++) {
              $(".imagediv").empty();
              var crystalBtn = $("<img>");
              crystalBtn.addClass("crystals")
              .attr("value", Math.floor(Math.random()*11+1))
              .attr("src", crystals[i]);
-
              $(".imagesdiv").append(crystalBtn);
          }
+
         }
 
-          
+        //FOR EACH CRYSTAL CLICKED, STORE THAT VALUE IN VAR totalScore
+      
+        //IF totalScore < random, THEN LET USER CONTINUE TO CLICK CRYSTALS
+        if (totalScore < random){
+            
+        } 
+        //ELSE IF totalScore === resetGame, print 'You won!', and wins++
+        else if (totalScore === random){
+
+        }
+        // ELSE print "You lost"
+
+        else{
+                
+        }  
+
+
+
+
+          //RESET "MATCH THIS VALUE" TO NEW RANDOM NUMBER
          function resetGame() {
-             random=Math.floor(Math.random()*101+19);
+           debugger;
+             random=Math.floor(Math.random()*102)+19;
              $("#randomNum").text(random);
              renderimg();
-         }
+            }
 
             resetGame();
-          //RANDOM # GENERATOR FOR CRYSTAL 1 BETWEEN 1-12. MUST BE HIDDEN
-          //RANDOM # GENERATOR FOR CRYSTAL 2 BETWEEN 1-12. MUST BE HIDDEN
-          //RANDOM # GENERATOR FOR CRYSTAL 3 BETWEEN 1-12. MUST BE HIDDEN
-          //RANDOM # GENERATOR FOR CRYSTAL 4 BETWEEN 1-12. MUST BE HIDDEN
-
-         
-         
-
-          //SECION FOR "YOUR TOTAL SCORE IS: "
-          
-
-          //SECTION FOR TOTAL SCORE NUMERICAL VALUE
+      
          
           
 });
