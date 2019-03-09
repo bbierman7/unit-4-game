2a4b9b89513a7e41d48183da54cc5d7f12e13b8c
bbierman <bbierman7@gmail.com>
2019-03-08 22:03:53 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,14 @@
 $(document).ready(function() {
 
-        
+var wins = 0;
+var losses = 0;
+var randomNum;
+var totalScore = 0;
+var restartGame;
 
-          //CRYSTAL ARRAY FOR BUTTONS
-          var crystals = ["assets/images/crystal-1.png", "assets/images/crystal-2.png", "assets/images/crystal-3.png", "assets/images/crystal-4.png"];
-          var random;
-          
-          //Variable named crystalBtn equal to $("<button>")
-        function renderimg() {
-            
-         for (let i = 0; i < crystals.length; i++) {
-             $(".imagediv").empty();
-             var crystalBtn = $("<img>");
-             crystalBtn.addClass("crystals")
-             .attr("value", Math.floor(Math.random()*11+1))
-             .attr("src", crystals[i]);
-             $(".imagesdiv").append(crystalBtn);
-         }
+//Generate random number and output it to screen
+function randomNumGen() {
+    randomNum=(Math.floor(Math.random() * (102)+ 19)
+    )}
 
-        }
-
-        //FOR EACH CRYSTAL CLICKED, STORE THAT VALUE IN VAR totalScore
-      
-        //IF totalScore < random, THEN LET USER CONTINUE TO CLICK CRYSTALS
-        if (totalScore < random){
-            
-        } 
-        //ELSE IF totalScore === resetGame, print 'You won!', and wins++
-        else if (totalScore === random){
-
-        }
-        // ELSE print "You lost"
-
-        else{
-                
-        }  
-
-
-
-
-          //RESET "MATCH THIS VALUE" TO NEW RANDOM NUMBER
-         function resetGame() {
-           debugger;
-             random=Math.floor(Math.random()*102)+19;
-             $("#randomNum").text(random);
-             renderimg();
-            }
-
-            resetGame();
-      
-         
-          
 });
