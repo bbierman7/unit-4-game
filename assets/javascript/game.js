8e42f577a7b5a594cb91e0b875956b0b9e0b18c5
bbierman <bbierman7@gmail.com>
2019-03-09 10:20:07 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,28 @@ $(document).ready(function() {
 
 var wins = 0;
 var losses = 0;
-var randomNum;
+var randomNum = 0;
 var totalScore = 0;
 var restartGame;
 
-//Generate random number and output it to screen
+
+//Generate random number and output it to text where Random Number is
 function randomNumGen() {
     randomNum=(Math.floor(Math.random() * (102)+ 19)
-    )}
+    )};
 
+$("#randomNum").appemd(randomNum);
+
+function randomCrystalGen() {
+    randomCrys=(Math.floor(Math.random() * (12)+ 1)
+    )
+    $("#crystalBtn1").click(function(){
+        $(this).append(randomCrys);
+        console.log(this)
+    });
+};
+
+
+
+   
 });
